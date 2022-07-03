2e7153908c2966c7b99a6f2393d2cadad7491764
diegodox <android.mxdiego9@gmail.com>
2022-07-03 15:20:25 +0900

    refactor fish.nix

--- fish/fish.nix
+++ fish/fish.nix
@@ -1,28 +1,32 @@
 { pkgs, ... }:
 
+let
+  oh-my-fish = {
+    name = "foreign-env";
+    src = pkgs.fetchFromGitHub {
+      owner = "oh-my-fish";
+      repo = "plugin-foreign-env";
+      rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+      sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+    };
+  };
+  fzf-fish = {
+    name = "fzf.fish";
+    src = pkgs.fetchFromGitHub {
+      owner = "PatrickF1";
+      repo = "fzf.fish";
+      rev = "6d8e962f3ed84e42583cec1ec4861d4f0e6c4eb3";
+      hash = "sha256-0rnd8oJzLw8x/U7OLqoOMQpK81gRc7DTxZRSHxN9YlM=";
+    };
+  };
+in
 {
   config = {
     programs.fish = {
       enable = true;
       plugins = [
-        {
-          name = "foreign-env";
-          src = pkgs.fetchFromGitHub {
-            owner = "oh-my-fish";
-            repo = "plugin-foreign-env";
-            rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
-            sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
-          };
-        }
-        {
-          name = "fzf.fish";
-          src = pkgs.fetchFromGitHub {
-            owner = "PatrickF1";
-            repo = "fzf.fish";
-            rev = "6d8e962f3ed84e42583cec1ec4861d4f0e6c4eb3";
-            hash = "sha256-0rnd8oJzLw8x/U7OLqoOMQpK81gRc7DTxZRSHxN9YlM=";
-          };
-        }
+        oh-my-fish
+        fzf-fish
       ];
 
       shellAbbrs = {
